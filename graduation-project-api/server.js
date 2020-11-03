62753a6883b39d6ab95894908dc9f7ceafd4d67b
Mazen-Al-Samman <mazenalsmman@gmail.com>
2020-11-03 22:53:43 +0200

    Edit the header of accepted requests

--- graduation-project-api/server.js
+++ graduation-project-api/server.js
@@ -4,19 +4,21 @@ const mongoose = require('mongoose');
 const Teacher = require('./models/teacher');
 const Student = require('./models/student');
 const Class = require('./models/class');
-mongoose.connect('mongodb+srv://mhd:123@classes.8mkn9.mongodb.net/graduation?retryWrites=true&w=majority').then(res=>{app.listen(3000)}).catch(e=>{console.log(e)});
-
+mongoose.connect('mongodb+srv://mazen:123@test.nt5v0.mongodb.net/graduation?retryWrites=true&w=majority').then(res=>{app.listen(8080)}).catch(e=>{console.log(e)});
 
 //middleware :
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 //routes
-
-app.post('/classes', (req,res)=>{
-    
+app.post('/classes', (req,res)=>{  
     const class1 = new Class({
         className: 'Java',
         students: [{
@@ -38,36 +40,27 @@ app.post('/classes', (req,res)=>{
             students:['ahmad','mazen','khaled']
         }]
     });
-    
-
     class1.save().then(response=>{ res.json(response)}).catch(e=>{res.status(400).json("error while saving the class data "+e)});
-
-    
-
-
 });
 
 
 app.post('/students', (req,res)=>{
-    
+    console.log("Request : ") ;
+    console.log(req.body) ;
     const std1 = new Student({
-        firstName: 'mazen',
-        lastName:'al-samman',
-        id_number:'212322',
-        password:'123',
-        faceID :'1923',
-        images:['asdffadff','adfadfwefk','adfasdfdsafadsf']
-
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        id_number:req.body.id_number,
+        password:req.body.password,
+        faceID :req.body.faceID,
+        images:req.body.images
     });
-
-    std1.save().then(response=>{ res.json(response)}).catch(e=>{res.status(400).json("error while saving the student data "+e)});
-
-    
-
-
+        std1.save().then(response=>{ res.json(response)}).catch(e=>{res.status(400).json("error while saving the student data "+e)});    
 });
 
-
+app.get('/students' , (req , res) => {
+    res.end("Hello From API") ;
+});
 
 
 
