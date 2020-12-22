2f0aeda5619d9901f4b67da1821ae3edc1501417
MHD22 <mhd.sad22@gmail.com>
2020-12-23 01:18:01 +0200

    move the create person to the back end .. still not working

--- graduation-project-api/server.js
+++ graduation-project-api/server.js
@@ -7,8 +7,8 @@ const Class = require('./models/class');
 const cors = require('cors');
 app.use(cors());
 
+const PORT  = process.env.PORT || 3000;
 
-mongoose.connect('mongodb+srv://mazen:123@test.nt5v0.mongodb.net/graduation?retryWrites=true&w=majority').then(res=>{app.listen(3000)}).catch(e=>{console.log(e)});
 
 
 //middleware :
@@ -24,6 +24,8 @@ app.use(function(req, res, next) {
 
 
 //routes
+
+app.post('/createPerson',createPersonHandler);
 
 app.get('/teacherClasses' , (req , res) => {
   const id = req.query.id ;
@@ -135,4 +137,104 @@ app.get('/students' , (req , res) => {
       });
 });
 
+mongoose.connect('mongodb+srv://mazen:123@test.nt5v0.mongodb.net/graduation?retryWrites=true&w=majority')
+.then(res=>{
+  app.listen(PORT,()=>{
+    console.log(`Listining on PORT: ${PORT}`)})
+  })
+.catch(e=>{console.log(e)});
 
+
+//helper functions :
+
+function createPersonHandler(req,res){
+  let {id_number,stName,file}= req.body;
+  //check the id in database.
+  console.log(file[0]);
+
+
+  createPersonOnLuxand(stName,file)
+  .then(result => {
+          let personID = result.id; // get the id for add faces to the same person.
+          addFaceTpPerson(file[1],personID)
+          .then(data =>{
+            addFaceTpPerson(file[2],personID)
+            .then(data=>{
+              listFacesOfPerson(file[0],personID)
+              .then(result => {
+                console.log(result)
+                res.json(result)})
+              .catch(error => console.log('error listing the faces of person..', error));
+            })
+          })
+          .catch(error => console.log('error within adding face to the person ', error));
+         
+          
+          //alert .. added successfully
+      })
+      .catch(error => console.log('error fetching (create person On API)', error));
+
+
+  
+
+
+}
+function createPersonOnLuxand(stName,file){
+
+
+  var formdata = new FormData();
+  formdata.append("name", stName);
+  formdata.append("photo", file[0], "file");
+  formdata.append("store", "1");
+
+  var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow'
+  };
+
+  return fetch("https://api.luxand.cloud/subject/v2", requestOptions)
+         .then(response => response.json())
+}
+function addFaceTpPerson(file,id){
+
+
+  var myHeaders = new Headers();
+        myHeaders.append("token", "0ed0d51e90cc4f3ab510a564cfb94b60");
+
+        var formdata = new FormData();
+        formdata.append("photo", file, "file");
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+        };
+        
+        return fetch(`https://api.luxand.cloud/subject/${id}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {})
+            .catch(error => console.log('error within adding  face to the person ', error));
+
+}
+
+function listFacesOfPerson(file,id){
+  var myHeaders = new Headers();
+  myHeaders.append("token", "{{api-token}}");
+  
+  var formdata = new FormData();
+  formdata.append("photo", file, "file");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    body: formdata,
+    redirect: 'follow'
+  };
+  
+  return fetch(`https://api.luxand.cloud/subject/${id}`, requestOptions)
+    .then(response => response.json())
+    
+}
