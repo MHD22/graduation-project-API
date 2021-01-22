c196d62f6754000b320a0ca684f2aa33816fd9db
MHD22 <mhd.sad22@gmail.com>
2021-01-22 15:59:01 +0200

    test server

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ const PORT = process.env.PORT || 3000;
 //App middlewares :
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 let upload = multer({ dest: 'uploads/' });
 app.use(cors());
 
